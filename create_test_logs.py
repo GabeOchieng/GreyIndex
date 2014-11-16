28f057025e0eb598a5f0f70c7e3f0522b614f1b6
Kostas Livieratos <kostas@warp.ly>
2014-11-16 21:28:15 +0200

    Added test logs creation script

---
+++ create_test_logs.py
@@ -0,0 +1,24 @@
+"""
+    Script to create test logs
+"""
+from datetime import datetime
+import time
+
+def create():
+    f = open('test.log', 'w')
+
+    for i in range(0, 100, 2):
+        f.write("ERROR : test\n")
+    for i in range(1, 101, 2):
+        f.write("WARNING : test\n")
+    for i in range(2, 102, 2):
+        f.write("INFO : test\n")
+
+    for i in range(0,100):
+        f.write("%s:%s\n" % (str(datetime.utcnow()), "test"))
+        time.sleep(0.5)
+
+    f.close()
+
+if "__main__" == __name__:
+    create()

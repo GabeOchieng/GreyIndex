0a957cad72bfdaf28e223c42e5be9aff96ce6a08
Kostas Livieratos <kostas@warp.ly>
2014-11-16 21:11:43 +0200

    Filled recbyseverity method

--- engine/recognition.py
+++ engine/recognition.py
@@ -7,13 +7,30 @@ class Recognition():
     def __init__(self):
         pass
 
+    @staticmethod
     def recbyseverity(self):
         """
         Tag logs by severity
         :return: Dictionary: {"info": [info_list], "error":[error_list], "warning":[warning_list]}
         """
-        pass
 
+        info_list = []
+        warning_list = []
+        error_list = []
+
+        with open("test.log") as f:
+            # Usage of readlines() is mandatory here 'cause of it's garbage collecting policy
+            content = f.readlines()
+            for line in content:
+                if "info" in line.lower():
+                    info_list.append(line)
+                elif "warning" in line.lower():
+                    warning_list.append(line)
+                elif "error" in line.lower():
+                    error_list.append(line)
+
+
+    @staticmethod
     def recbytimestamp(self):
         """
         Tag logs by timestamp
